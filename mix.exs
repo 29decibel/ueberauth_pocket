c6814b93619255e72cc4d7d4155baa7e76558533
29decibel <m@idots.me>
2023-10-01 16:36:30 -0700

    upgrade httpoison

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule UeberauthPocket.Mixfile do
   defp deps do
     [
       {:ueberauth, "~> 0.4"},
-      {:httpoison, "~> 1.8"},
+      {:httpoison, "~> 2.0"},
       {:jason, "~> 1.2"},
       {:credo, "~> 0.5", only: [:dev, :test]},
       # docs dependencies
